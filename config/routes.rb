fd0f43481a296e205c50a355b81a4d8012f61cb5
atsushi7534 <atsushi1007@outlook.jp>
2021-01-05 14:02:21 +0000

    interval

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
+  root 'books#index'
+  devise_for :users
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
 end
